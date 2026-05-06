264f57db09bc2b974c3c080e8631d893927bd58a
manndyjohnson-creator <manndyjohnson@gmail.com>
2026-05-06 18:47:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const overlay = document.getElementById('overlay');
     const cancelBtn = document.getElementById('cancel-btn');
 
-    // Show popup after 30 seconds
+    // Show popup after 1.5 seconds
     setTimeout(() => {
         popup.classList.add('active');
         overlay.classList.add('active');
